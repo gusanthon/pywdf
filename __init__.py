316f9de8928f767bb913077a5404a3c6ff14b392
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-01-17 21:35:54 -0500

    Import all subpackages for pywdf in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,17 @@
-from wdf import *
-from rtype import *
-from circuit import *
+
+from .core.circuit import *
+from .core.rtype import *
+from .core.wdf import *
+
+from .examples.tr_808_hatresonator import TR_808_HatResonator
+from .examples.bassmantonestack import BassmanToneStack
+from .examples.baxandalleq import BaxandallEQ, UnadaptedBaxandallEQ
+from .examples.diodeclipper import DiodeClipper
+from .examples.passivelpf import PassiveLPF
+from .examples.rca_mk2_sef import RCA_MK2_SEF
+from .examples.rclowpass import RCLowPass
+from .examples.voltagedivider import VoltageDivider
+
+
+__all__ = ['examples', 'lib']
+

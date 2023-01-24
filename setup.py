09ef4e5be90e8ecc57cd6997c325bf26705a767b
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-01-24 14:13:43 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh
+
+setuptools.setup(
+    name='pywdf',
+    version='0.0.1',
+    author='Gustav Anthon',
+    author_email='anthon.gus1@gmail.com',
+    description='A framework for wave digital filter circuits',
+    long_description=long_description,
+    url= 'https://github.com/gusanthon/pywdf',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

b24ab4e9c831a570912661d85143d0fafdec16bc
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-01-24 14:36:34 -0500

    Bug fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     author_email='anthon.gus1@gmail.com',
     description='A framework for wave digital filter circuits',
     long_description=long_description,
-    url= 'https://github.com/gusanthon/pywdf',
+    url= setuptools.find_packages(),
     packages=["pywdf"],
     classifiers=[
         "Programming Language :: Python :: 3",

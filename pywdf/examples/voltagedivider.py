3cf51dc2ab809949cdde458adb18af3fab299da7
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-01-24 14:59:00 -0500

    Bug fix

--- pywdf/examples/voltagedivider.py
+++ pywdf/examples/voltagedivider.py
@@ -1,7 +1,5 @@
 import sys
 import os
-import schemdraw
-import schemdraw.elements as e
 
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
@@ -26,22 +24,6 @@ class VoltageDivider(Circuit):
     def set_R2(self,new_R):
         self.R2.set_resistance(new_R)
 
-    def show_schematic(self, outpath=None):
-        d = schemdraw.Drawing(fontsize=12)
-        d.add(e.DOT, label='Input')
-
-        R1 = d.add(e.RES, d='right', label='R1')
-        R2 = d.add(e.RES, d='down', label='R2')
-        d.add(e.GND)
-
-        d.add(e.LINE, d='right', l=1, xy = R1.end)
-        d.add(e.DOT, label='Output')
-        d.draw()
-        if outpath:
-            d.save(outpath)
-
 if __name__ == '__main__':
-    vd = VoltageDivider(44100, 1e12, 1e10)
-    # vd.plot_freqz()
-    # vd.show_schematic()
-    vd.AC_transient_analysis()
+    vd = VoltageDivider(44100, 1e5, 1e10)
+    vd.plot_freqz()

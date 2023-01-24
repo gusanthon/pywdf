7e52b0b39649246e03be386e5549926cc4bfb641
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-01-24 14:50:46 -0500

    testing setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh
 
 setuptools.setup(
-    name='pywdf',
-    version='0.0.1',
-    author='Gustav Anthon',
-    author_email='anthon.gus1@gmail.com',
-    description='A framework for wave digital filter circuits',
+    name="pywdf",
+    version="0.0.1",
+    author="Gustav Anthon",
+    author_email="anthon.gus1@gmail.com",
+    description="A framework for wave digital filter circuits",
     long_description=long_description,
-    url= 'https://github.com/gusanthon/pywdf',
-    packages=["pywdf"],
+    url= "https://github.com/gusanthon/pywdf",
+    packages=[setuptools.find_packages()],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2.7",

efb3dafa3cb7f79266f8cf4affbc45d00b7dffaa
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-01-26 14:15:03 -0500

    make fixed cutoff controls continuous

--- pywdf/examples/rca_mk2_sef.py
+++ pywdf/examples/rca_mk2_sef.py
@@ -21,6 +21,8 @@ class RCA_MK2_SEF(Circuit):
         self.LP_mod = True
         self.HP_mod = True
 
+        self.k = 560
+
         # C & L vals for each HP & LP fc
 
         self.HP_vals = {
@@ -135,12 +137,14 @@ class RCA_MK2_SEF(Circuit):
         return k * super().process_sample(sample)
 
     def set_highpass_cutoff(self, new_cutoff):
-        self.highpass_cutoff, vals = self.get_closest(self.HP_vals, new_cutoff)
-        self._set_HP_components(vals['C'], vals['L'])
+        C = np.sqrt(2) / (self.k * new_cutoff * 2. * np.pi)
+        L = self.k / (2. * np.sqrt(2) * new_cutoff * 2. * np.pi)
+        self._set_HP_components(C,L)
 
     def set_lowpass_cutoff(self, new_cutoff):
-        self.lowpass_cutoff, vals = self.get_closest(self.LP_vals, new_cutoff)
-        self._set_LP_components(vals['C'], vals['L'])
+        C = (2 * np.sqrt(2)) / (self.k * new_cutoff * 2. * np.pi)
+        L = (np.sqrt(2) * self.k) / (new_cutoff * 2. * np.pi)
+        self._set_LP_components(C,L)
 
     def set_lowpass_knob_position(self, position):
         for i, fc in enumerate(self.LP_vals):
@@ -217,11 +221,14 @@ class RCA_MK2_SEF(Circuit):
 
 if __name__ == '__main__':
 
-    mk2 = RCA_MK2_SEF(44100, 175, 3990)
+    mk2 = RCA_MK2_SEF(44100, 0, 20e3)
+    # mk2.plot_freqz()
+    vals = range(0, 6000, 1000)
+    mk2.plot_freqz_list(vals, mk2.set_highpass_cutoff, 'hp cutoff')
 
-    mk2.plot_freqz_list(range(1,len(mk2.HP_vals)), mk2.set_highpass_knob_position, param_label='hpf knob pos')
+    # mk2.plot_freqz_list(range(1,len(mk2.HP_vals)), mk2.set_highpass_knob_position, param_label='hpf knob pos')
 
-    mk2.set_highpass_cutoff(170)
+    # mk2.set_highpass_cutoff(170)
 
-    mk2.plot_freqz_list(range(1,len(mk2.LP_vals)), mk2.set_lowpass_knob_position, param_label='lpf knob pos')
+    # mk2.plot_freqz_list(range(1,len(mk2.LP_vals)), mk2.set_lowpass_knob_position, param_label='lpf knob pos')
 

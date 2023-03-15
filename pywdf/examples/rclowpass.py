6b3c493995c448850a4636dbad54f54ade673e5d
Xavier Lizarraga <xavilizarraga@gmail.com>
2023-03-15 15:22:01 +0100

    Fix rclowpass example.

--- pywdf/examples/rclowpass.py
+++ pywdf/examples/rclowpass.py
@@ -1,14 +1,17 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(
+    os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
 
 from core.wdf import *
 from core.circuit import Circuit
 
+
 class RCLowPass(Circuit):
     def __init__(self, sample_rate: int, cutoff: float) -> None:
-    
+
         self.fs = sample_rate
         self.cutoff = cutoff
 
@@ -16,11 +19,11 @@ class RCLowPass(Circuit):
         self.R = 1.0 / (2 * np.pi * self.C * self.cutoff)
 
         self.R1 = Resistor(self.R)
-        self.C1 = Capacitor(self.C,self.fs)
+        self.C1 = Capacitor(self.C, self.fs)
 
-        self.S1 = SeriesAdaptor(self.R1,self.C1)
+        self.S1 = SeriesAdaptor(self.R1, self.C1)
         self.I1 = PolarityInverter(self.S1)
-        self.Vs = IdealVoltageSource(self.S1)
+        self.Vs = IdealVoltageSource(self.I1)
 
         super().__init__(self.Vs, self.Vs, self.C1)
 
@@ -30,8 +33,9 @@ class RCLowPass(Circuit):
             self.R = 1.0 / (2 * np.pi * self.C * self.cutoff)
             self.R1.set_resistance(self.R)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     lpf = RCLowPass(44100, 1000)
-    # lpf.plot_freqz()
-    # lpf.plot_freqz_list(range(1000,10000,1000),lpf.set_cutoff,param_label='cutoff')
+    lpf.plot_freqz()
+    lpf.plot_freqz_list(range(1000, 10000, 1000), lpf.set_cutoff, param_label="cutoff")
     lpf.AC_transient_analysis()

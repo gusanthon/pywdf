807ae65e1f9beac9099a12c60060d0ace3e0470b
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-04-25 17:41:19 -0400

    Update test file

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
-from examples import *
+from pywdf import RCA_MK2_SEF, TR_808_HatResonator, DiodeClipper
 
-# sweep knob positions of RCA mk2 SEF low pass filter
-mk2_sef = rca_mk2_sef.RCA_MK2_SEF(44100, 0, 3000)
+# sweep positions of RCA mk2 SEF low pass filter knob and plot frequency responses
+mk2_sef = RCA_MK2_SEF(44100, 0, 3000)
 positions = range(1,11)
-mk2_sef.plot_freqz_list(positions, mk2_sef.set_lowpass_knob_position, param_label = 'lpf knob pos')
- 
-# sweep resonance values in tr 808 hat resonator
-hr = tr_808_hatresonator.TR_808_HatResonator(44100, 1000, .5)
-resonances = [.1,.2,.3,.4,.5,.6,.7,.8,.9]
-hr.plot_freqz_list(resonances, hr.set_resonance, param_label= 'resonance val')
+mk2_sef.plot_freqz_list(positions, mk2_sef.set_lowpass_knob_position, param_label = 'lpf knob pos', outpath='pywdf/images/mk2_sef_lpf_knob.png')
 
-# visualize transient response of Diode Clipper to AC signal
-dc = diodeclipper.DiodeClipper(44100, cutoff= 1000, input_gain_db = 5)
-dc.AC_transient_analysis()
+# visualize impulse response of TR-808 hat resonator
+hr = TR_808_HatResonator(44100, 1000, .5)
+hr.plot_impulse_response(outpath='pywdf/images/hat_res_IR.png')
+
+# analyze transient response of Diode Clipper to AC signal
+dc = DiodeClipper(44100, cutoff= 1000, input_gain_db = 5)
+dc.AC_transient_analysis(outpath='pywdf/images/diode_clipper_transient_anal.png')

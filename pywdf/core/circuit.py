d7a8d176c1a81ed189159470c337aad09ec7914a
gusanthon <99610799+gusanthon@users.noreply.github.com>
2023-04-25 17:27:44 -0400

    Add plot_impulse_response

--- pywdf/core/circuit.py
+++ pywdf/core/circuit.py
@@ -79,6 +79,16 @@ class Circuit:
         d = np.zeros(int(delta_dur * self.fs))
         d[0] = amp
         return self.process_signal(d)
+    
+    def plot_impulse_response(self, n_samples: int = 500, outpath: str = None, delta_dur: float = 1, amp: float = 1) -> None:
+        plt.plot(self.get_impulse_response(delta_dur, amp)[:n_samples])
+        plt.xlabel('Sample [n]')
+        plt.ylabel('Amplitude [V]')
+        plt.title(f'{self.__class__.__name__} impulse response')
+        plt.grid()
+        if outpath:
+            plt.savefig(outpath)
+        plt.show()
 
     def set_sample_rate(self, new_fs: float) -> None:
         """Change system's sample rate
